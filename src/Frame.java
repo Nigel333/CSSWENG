7c01a656fcf8c05569205285aa1b184899fe83c7
Nigel <nigel_valero@dlsu.edu.ph>
2024-02-05 20:37:55 +0800

    margin for middle

--- src/Frame.java
+++ src/Frame.java
@@ -5,23 +5,39 @@ import java.util.ArrayList;
 
 public class Frame extends JFrame {
     JTable partTable;
-    JPanel leftPanel, rightPanel, middlePanel;
+    JPanel marginPanel, leftPanel, rightPanel, middlePanel;
     JPanel filterPanel;
     JTextField searchField;
 
     public Frame(Main model) {
         super("Item Database");
 
+        // initializing JTable with custom column size
         PartTableModel tableModel = new PartTableModel(model.filteredParts);
         partTable = new JTable(tableModel);
-
+        partTable.setPreferredScrollableViewportSize(new Dimension(200,400));
+        partTable.getColumnModel().getColumn(0).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(1).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(2).setPreferredWidth(20);
+        partTable.getColumnModel().getColumn(3).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(4).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(5).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(6).setPreferredWidth(10);
+        partTable.getColumnModel().getColumn(7).setPreferredWidth(10);
         // Set the layout manager for the frame
         setLayout(new BorderLayout());
 
+        marginPanel = new JPanel(new BorderLayout());
+        marginPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        marginPanel.setBackground(Color.GREEN);
+
         middlePanel = new JPanel(new BorderLayout());
+        middlePanel.setBackground(Color.GREEN);
+        middlePanel.setPreferredSize(new Dimension(300,300));
         searchField = new JTextField();
         middlePanel.add(searchField, BorderLayout.NORTH);
-        middlePanel.add(new JScrollPane(partTable), BorderLayout.CENTER);
+        marginPanel.add(new JScrollPane(partTable), BorderLayout.CENTER);
+        middlePanel.add(marginPanel, BorderLayout.CENTER);
         add(middlePanel, BorderLayout.CENTER);
         initializeLeft();
         add(leftPanel, BorderLayout.WEST);
